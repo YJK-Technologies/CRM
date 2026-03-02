bf76326ae1e0c9899cf6b9f98decb78f3163a266
Sakthi0704 <sakthiganesh.j@yjktechnologies.com>
2026-03-02 14:18:35 +0530

    Bug fixing on 3/2/20206

--- src/Apiconfig.js
+++ src/Apiconfig.js
@@ -1,6 +1,6 @@
 const config = {
     apiBaseUrl: 'https://erpdev.yjktechnologies.com:5577' 
-    // apiBaseUrl: 'http://95.216.47.253:5577'
+    // apiBaseUrl: 'http://localhost:5577'
   };
   
   module.exports = config;
