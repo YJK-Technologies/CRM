46496fe5aa4415f72ee6b373370f9739f8cb2141
Dinesh-Gokul <dinesh.gokul@avvaitech.com>
2026-09-17 14:57:25 +0530

    17-09-2026 Evening Changes

--- src/Input.js
+++ src/Input.js
@@ -8,6 +8,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import './apps.css'
 import Select from 'react-select'
 import LoadingScreen from './Loading';
+import DefaultProductImage from "./DefaultIMG/Product.png";
 
 const config = require('./Apiconfig');
 
@@ -1094,6 +1095,7 @@ const handleNavigate = () => {
                   </div>
                   </div>
                 </div>
+                <div class="row">
                 <div className="col-md-3 form-group mb-2">
                   <div class="exp-form-floating">
                     <label for="locno" class="exp-form-labels">
@@ -1108,7 +1110,7 @@ const handleNavigate = () => {
                     />
                   </div>
                 </div>
-                {selectedImage && (
+                {/* {selectedImage && (
                   <div className="col-md-3 form-group mb-2">
                     <div class="exp-form-floating">
                       <img
@@ -1119,7 +1121,17 @@ const handleNavigate = () => {
                       />
                     </div>
                   </div>
-                )}
+                )} */}
+                <div className="col-md-3 form-group mb-2">
+
+                  <div className="image-preview-frame">
+                    <img
+                      src={selectedImage || DefaultProductImage}
+                      alt="Selected Preview"
+                      className="preview-image"
+                    />
+                  </div>
+                </div>
                 <div className="col-md-3 form-group mb-2">
                   <div class="exp-form-floating">
                     <label for="locno" class="exp-form-labels">
@@ -1143,7 +1155,7 @@ const handleNavigate = () => {
                     />
                   </div>
                 </div>
-                {selectedSignatureImage && (
+                {/* {selectedSignatureImage && (
                   <div className="col-md-3 form-group mb-2">
                     <div class="exp-form-floating">
                       <img
@@ -1154,48 +1166,19 @@ const handleNavigate = () => {
                       />
                     </div>
                   </div>
-                )}
-                {/* <div className="col-md-3 form-group  mb-2">
-                  {mode === "create" ? (
-                    <div class="exp-form-floating">
-                      <div class="d-flex justify-content-start">
-                        <div>
-                          <label for="state" class="exp-form-labels">
-                            Created By
-                          </label>
-                        </div>
-                      </div>
-                      <input
-                        id="emailid"
-                        class="exp-input-field form-control"
-                        type="text"
-                        placeholder=""
-                        required
-                        title="Please enter the email ID"
-                        value={created_by}
-                      />
-                    </div>
-                  ) : (
-                    <div class="exp-form-floating">
-                      <div class="d-flex justify-content-start">
-                        <div>
-                          <label for="state" class="exp-form-labels">
-                            Modified By
-                          </label>
-                        </div>
-                      </div>
-                      <input
-                        id="emailid"
-                        class="exp-input-field form-control"
-                        type="text"
-                        placeholder=""
-                        required
-                        title="Please enter the email ID"
-                        value={modified_by}
-                      />
-                    </div>
-                  )}
-                </div> */}
+                )} */}
+                <div className="col-md-3 form-group mb-2">
+
+                  <div className="image-preview-frame">
+                    <img
+                      src={selectedSignatureImage || DefaultProductImage}
+                      alt="Selected Preview"
+                      className="preview-image"
+                    />
+                  </div>
+                </div>
+                </div>
+                
                 <div class="col-md-3 form-group d-flex justify-content-start mb-4">
                   {mode === "create" ? (
                     <button onClick={handleInsert} className="mt-4" title="Save">
